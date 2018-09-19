318860506c707d21baacc72e8716af1ff3f4dbcd
BMSalamander <hostan3@gmail.com>
2018-09-19 17:17:56 +0500

    Антипьев Александр. Задание 11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -11,8 +11,30 @@ public class Task11Main {
          */
     }
 
+    static int numMin(int[] arr) {
+        int minEl = arr[0];
+        int minIndex = 0;
+
+        for (int i = 0; i < arr.length; ++i) {
+            if (minEl > arr[i]) {
+                minEl = arr[i];
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
+
     static void swap(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0)
+            return;
+
+        int tmp = arr[0];
+        int indexOfMin = numMin(arr);
+
+        arr[0] = arr[indexOfMin];
+        arr[indexOfMin] = tmp;
     }
 
 }
