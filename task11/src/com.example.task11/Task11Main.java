78387d4002fd94ed2e133fbf2170804261e21d76
Leva <petrosian.lyova@yandex.ru>
2018-09-22 06:28:29 +0500

    Петросян. Task11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,5 +1,7 @@
 package com.example.task11;
 
+import java.util.Arrays;
+
 public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -12,7 +14,22 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        int min = Integer.MAX_VALUE;
+        int index = 0, temp;
+
+        if (arr == null || arr.length == 0)
+            return;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (min >= arr[i]) {
+                min = arr[i];
+                index = i;
+            }
+        }
+
+        temp = arr[0];
+        arr[0] = arr[index];
+        arr[index] = temp;
     }
 
 }
