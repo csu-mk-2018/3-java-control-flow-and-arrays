46adf4086a6e10222f684f2739778c704055e3c7
Leva <petrosian.lyova@yandex.ru>
2018-09-22 07:01:34 +0500

    Петросян. Task12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -11,8 +11,33 @@ public class Task12Main {
          */
     }
 
+    static int[] getMinElementAndIndex (int[] arr, int startIndex) {
+        int min = Integer.MAX_VALUE;
+        int index = 0;
+
+        for (int i = startIndex; i < arr.length; i++)
+            if (min >= arr[i]) {
+                min = arr[i];
+                index = i;
+            }
+
+        return new int[]{min, index};
+    }
+
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+
+        int[] result;
+        int temp;
+
+        if (arr == null || arr.length == 0)
+            return;
+
+        for (int i = 0; i < arr.length; i++){
+            result = getMinElementAndIndex(arr, i);
+            temp = arr[i];
+            arr[i] = result[0];
+            arr[result[1]] = temp;
+        }
     }
 
 }
