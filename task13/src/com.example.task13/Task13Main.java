dc714ec5ff1acc9dafad7f115b6430377afbaa3a
Leva <petrosian.lyova@yandex.ru>
2018-09-23 20:27:58 +0500

    Петросян. Task13

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -1,19 +1,43 @@
 package com.example.task13;
 
+import com.sun.deploy.util.ArrayUtil;
+
 public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+       int [] arr2 =  removeMoreThen1000(arr);
+        System.out.println(java.util.Arrays.toString(arr2));
     }
 
-    static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+    static  int getNewSize(int[] arr){
+
+        int size = 0;
+
+        for (int elem:arr) {
+            if (elem > 1000)
+                size++;
+        }
+
+        return  size;
+    }
+
+   static int[] removeMoreThen1000(int[] arr) {
+
+        if (arr == null || arr.length == 0)
+            return arr;
+
+        int[] newMass = new int[arr.length - getNewSize(arr)];
+
+        for (int i = 0, j = 0; i < arr.length; i++)
+           if (arr[i] <= 1000){
+               newMass[j] = arr[i];
+               j++;
+           }
+
+        return  newMass;
     }
 
 }
