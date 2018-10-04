fe7d7f10399ecde184d45e0941292fef6b97232b
Vadim Metnyov <metnyov@gmail.com>
2018-10-04 09:08:31 +0500

    task08

--- task08/src/com.example.task08/Task08Main.java
+++ task08/src/com.example.task08/Task08Main.java
@@ -13,8 +13,15 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        if (arr.length == 0) {
+            return 0;
+        } else {
+            long res = 1;
+            for (int it : arr) {
+                res *= it;
+            }
+            return res;
+        }
     }
 
 }
