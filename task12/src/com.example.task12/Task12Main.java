ef28a085041a8993466cb0396313b7f671c21081
PetrosyanMk15 <43194832+PetrosyanMk15@users.noreply.github.com>
2018-10-08 18:01:27 +0500

    Update Task12Main.java

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -11,32 +11,32 @@ public class Task12Main {
          */
     }
 
-    static int[] getMinElementAndIndex (int[] arr, int startIndex) {
+    static int getMinElementIndex(int[] arr, int startIndex) {
         int min = Integer.MAX_VALUE;
-        int index = 0;
+        int index = -1;
 
         for (int i = startIndex; i < arr.length; i++)
-            if (min >= arr[i]) {
+            if (min > arr[i]) {
                 min = arr[i];
                 index = i;
             }
 
-        return new int[]{min, index};
+        return index;
     }
 
     static void selectionSort(int[] arr) {
 
-        int[] result;
+        int minIndex;
         int temp;
 
         if (arr == null || arr.length == 0)
             return;
 
-        for (int i = 0; i < arr.length; i++){
-            result = getMinElementAndIndex(arr, i);
+        for (int i = 0; i < arr.length; i++) {
+            minIndex = getMinElementIndex(arr, i);
             temp = arr[i];
-            arr[i] = result[0];
-            arr[result[1]] = temp;
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
     }
 
