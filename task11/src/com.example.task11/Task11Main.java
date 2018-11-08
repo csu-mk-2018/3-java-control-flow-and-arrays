e46acb388e3f211e1b98093cf5462943e897c729
DanilaKroshin <skozlozopsk74@gmail.com>
2018-11-08 18:47:05 +0500

    Крошин Д. И. Task11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,22 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
+        if (arr != null) {
+            if (arr.length > 0) {
+                int min = Integer.MAX_VALUE;
+                int minIndex = 0;
+                for (int i = 0; i < arr.length; ++i) {
+                    if (arr[i] < min) {
+                        min = arr[i];
+                        minIndex = i;
+                    }
+                }
+                int toMem = arr[0];
+                arr[0] = arr[minIndex];
+                arr[minIndex] = toMem;
 
+                //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+            }
+        }
+    }
 }
